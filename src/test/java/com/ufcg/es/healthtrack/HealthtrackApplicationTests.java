bcf5ae5d999e80cacbc41ed00fc6dce9c6448aba
Renildo <renildo.melo@ccc.ufcg.edu.br>
2022-03-23 00:36:08 -0300

    Criados testes de unidade para ExameFezesService.

--- src/test/java/com/ufcg/es/healthtrack/HealthtrackApplicationTests.java
+++ src/test/java/com/ufcg/es/healthtrack/HealthtrackApplicationTests.java
@@ -2,13 +2,16 @@ package com.ufcg.es.healthtrack;
 
 
 import com.ufcg.es.healthtrack.unit.service.ColesterolServiceTest;
+import com.ufcg.es.healthtrack.unit.service.ExameFezesServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ColesterolServiceTest.class})
+@Suite.SuiteClasses({
+        ColesterolServiceTest.class,
+        ExameFezesServiceTest.class})
 class HealthtrackApplicationTests {
 
 
---
+++ src/test/java/com/ufcg/es/healthtrack/unit/service/ExameFezesServiceTest.java
@@ -0,0 +1,79 @@
+package com.ufcg.es.healthtrack.unit.service;
+
+import com.ufcg.es.healthtrack.exception.HealthTrackSystemException;
+import com.ufcg.es.healthtrack.model.Usuario;
+import com.ufcg.es.healthtrack.model.dto.fezes.FezesDTO;
+import com.ufcg.es.healthtrack.model.exame.ExameFezes;
+import com.ufcg.es.healthtrack.repository.FezesRepository;
+import com.ufcg.es.healthtrack.service.ExameFezesService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExameFezesServiceTest {
+
+    private final int UMA_CHAMADA = 1;
+
+    @Mock
+    private FezesRepository fezesRepository;
+
+    @InjectMocks
+    private ExameFezesService exameFezesService;
+
+    private FezesDTO fezesDTO;
+    private ExameFezes exameFezes;
+    private Usuario usuario;
+    private List<ExameFezes> exameFezesList;
+
+    @Before
+    public void setUp() {
+        fezesDTO = new FezesDTO("descricao",LocalDateTime.now(),"aspectGeral","protozoarios","helmintos","metodo","observacoes");
+        usuario = new Usuario("email@email.com","meu nome","minha Senha");
+        exameFezes = new ExameFezes(usuario, "descricao",LocalDateTime.now(),"aspectGeral","protozoarios","helmintos","metodo","observacoes");
+        exameFezes.setId(1L);
+        exameFezesList = new ArrayList<>();
+        exameFezesList.add(exameFezes);
+    }
+
+    @Test
+    public void adicionaDadosDeveExecutarSaveApenasUmVez() {
+        exameFezesService.adicionaExame(fezesDTO,usuario);
+        verify(fezesRepository, times(UMA_CHAMADA)).save(any());
+    }
+
+    @Test
+    public void listarTodosPorUsuarioDeveExecutarFindAllByUsuarioApenasUmVez() {
+        when(fezesRepository.findAllByUsuario(usuario)).thenReturn(exameFezesList);
+        exameFezesService.listarTodosPorUsuario(usuario);
+        verify(fezesRepository, times(UMA_CHAMADA)).findAllByUsuario(any());
+    }
+
+    @Test
+    public void visualizarExameDeveExecutarFindByIdAndUsuarioApenasUmVez() {
+        when(fezesRepository.findByIdAndUsuario(1L,usuario)).thenReturn(Optional.of(exameFezes));
+        exameFezesService.visualizarExame(1L,usuario);
+        verify(fezesRepository, times(UMA_CHAMADA)).findByIdAndUsuario(anyLong(),any());
+    }
+    @Test
+    public void visualizarExameDeveLancarUmaExcesaoQuandoOExameNaoExistir() {
+        when(fezesRepository.findByIdAndUsuario(1L,usuario)).thenReturn(Optional.empty());
+        assertThrows(HealthTrackSystemException.class, () -> exameFezesService.visualizarExame(1L,usuario));
+        verify(fezesRepository, times(UMA_CHAMADA)).findByIdAndUsuario(anyLong(),any());
+    }
+}
