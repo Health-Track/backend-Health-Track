248073c2b5f6d1b3594ff9da951877da8c72784b
Renildo <renildo.melo@ccc.ufcg.edu.br>
2022-03-25 13:56:41 -0300

    Modificando HealthtrackApplicationTests para executar todos os testes de unidade e integração

--- src/test/java/com/ufcg/es/healthtrack/HealthtrackApplicationTests.java
+++ src/test/java/com/ufcg/es/healthtrack/HealthtrackApplicationTests.java
@@ -1,7 +1,5 @@
 package com.ufcg.es.healthtrack;
 
-
-import com.ufcg.es.healthtrack.unit.service.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ColesterolServiceTest.class,
-        ExameFezesServiceTest.class,
-        ExameUrinaServiceTest.class,
-        GlicemiaServiceTest.class,
-        HemogramaServiceTest.class,
-        PressaoServiceTest.class,
-        UsuarioServiceTest.class})
+        RunUnitTests.class,
+        RunE2ETests.class})
 class HealthtrackApplicationTests {
 
 
