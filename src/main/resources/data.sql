e6a587158c90574c21077e6d7b4f018fe445fd5e
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2022-03-27 20:15:59 -0300

    editing sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
-insert into usuario(nome, email, senha) values ('Luiz Bonfim', 'luiz@email.com', '12345678');
+insert into usuario(nome, email, senha) values ('José Mock', 'mock@email.com', '12345678');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (10, 15, 17, 28, '2022-02-23T08:10:12', 121, 'luiz@email.com');
+values (10, 15, 17, 28, '2022-02-23T08:10:12', 121, 'mock@email.com');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (11, 12, 18, 22, '2022-03-23T08:10:12', 150, 'luiz@email.com');
+values (11, 12, 18, 22, '2022-03-23T08:10:12', 150, 'mock@email.com');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (18, 30, 11, 29, '2022-04-23T08:10:12', 105, 'luiz@email.com');
+values (18, 30, 11, 29, '2022-04-23T08:10:12', 105, 'mock@email.com');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (15, 22, 23, 30, '2022-05-23T08:10:12', 106, 'luiz@email.com');
+values (15, 22, 23, 30, '2022-05-23T08:10:12', 106, 'mock@email.com');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (13, 20, 45, 31, '2022-06-23T08:10:12', 122, 'luiz@email.com');
+values (13, 20, 45, 31, '2022-06-23T08:10:12', 122, 'mock@email.com');
 
 insert into colesterol(colesterolhdl, colesterolldl, colesterol_naohdl, colesterol_total, data_medicao, relacao_totalhdl, usuario_email)
-values (12, 17, 29, 32, '2022-07-23T08:10:12', 133, 'luiz@email.com');
+values (12, 17, 29, 32, '2022-07-23T08:10:12', 133, 'mock@email.com');
 
